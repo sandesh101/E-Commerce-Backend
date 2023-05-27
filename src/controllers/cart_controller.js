90857a18efc195c1f4f0c3511b474e52ddd0948b
sandesh101 <rimal.sandesh11@gmail.com>
2023-05-27 18:06:49 +0545

    Build Cart

--- src/controllers/cart_controller.js
+++ src/controllers/cart_controller.js
@@ -1,15 +1,32 @@
 const CartModel = require("./../models/cart_model");
 
 const CartController = {
+
+    //Get cart for user
+    getCartForUser: async function(req,res){
+        try{
+            const foundCart = await CartModel.findOne({ user: user });
+
+            if(!foundCart){
+                res.json({ success: true, message: [] })
+            }
+            res.json({ success: true, data: foundCart.items })
+        }
+        catch(e){
+            res.json({ success: false, message: "Error for getting user cart" })
+        }
+    },
+
+
     addToCart: async function(req,res){
         try{
             const { product, user, quantity } = req.body;
-            const foundCart = CartModel.findOne({ user: user });
+            const foundCart = await CartModel.findOne({ user: user });
             // console.log(`CART: ${foundCart}`);
 
             //If cart doest exists
             if(!foundCart){
-                const newCart = new CartModel({ user: user });
+                const newCart = new CartModel({ user: user })
                 newCart.items.push({
                     product: product,
                     quantity: quantity
@@ -24,6 +41,7 @@ const CartController = {
                 {$push: {items: { product: product, quantity: quantity } } },
                 {new: true}
                 );
+                // await updatedCart.save();
             console.log(updatedCart);
             return res.json({ success: true, data: updatedCart, message: "Product Updated Successfully" })
 
@@ -35,7 +53,15 @@ const CartController = {
     removeCart: async function(req, res){
         try{
 
-        }catch(e){
+            const { user, product } = req.body;
+            const updatedCart = await CartModel.findOneAndUpdate(
+                {user: user},
+                {$pull: { items: { product:  product } } },
+                { new: true }
+            );
+            return res.json({ success: true, data: updatedCart, message: 'Product Removed Successfully' })
+        }
+        catch(e){
             res.json({ success: false, message: e.toString() })
         }
     }
--- src/routes/cart_routes.js
+++ src/routes/cart_routes.js
@@ -1,6 +1,8 @@
 const CartRouter = require("express").Router();
 const CartController = require("./../controllers/cart_controller");
 
+CartRouter.get("/:user", CartController.getCartForUser);
 CartRouter.post("/", CartController.addToCart);
+CartRouter.delete("/", CartController.removeCart);
 
 module.exports = CartRouter;
